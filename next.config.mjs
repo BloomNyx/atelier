465f4dc079c5de733698b24767e52ac5f9e88a8c
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-09 16:47:48 +0900

    atelier

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/atelier/',      // ★ 너의 GitHub Repository 이름
+  assetPrefix: '/atelier/',  // ★ 반드시 동일하게
 };
 
 export default nextConfig;
